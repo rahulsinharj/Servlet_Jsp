565e8e50ed70bd3652fa359e7b792ab3c10f1239
rahul <rahul@192.168.1.6>
2022-02-13 14:01:19 +0530

    MyServlet_ContextConfig

--- MyServlet/src/main/java/com/myservlet/MyServlet_ContextConfig.java
+++ MyServlet/src/main/java/com/myservlet/MyServlet_ContextConfig.java
@@ -17,9 +17,13 @@ import jakarta.servlet.http.HttpServletResponse;
 						
   ServletContext - is an obj which will be shared by ALL the servlets. So if you have multiple servlets which will share any same value of any variable/property, then go with ServletContext. 
   ServletConfig - If we have 5 servlets, then in total we will have 5 diff ServletConfig object;  but we will have only one shared ServletContext obj. So if you have different values for different servlets of any property, good to go for ServletConfig.
+	
+ # How to RUN this for checking MyServlet_ContextConfig woking ??
+   Ans = RightClick application "MyServlet" -> Run as -> Run on Server 
+   		 Change the url to http://localhost:2000/MyServlet/home
 				
 */
-public class MyServlet extends HttpServlet {
+public class MyServlet_ContextConfig extends HttpServlet {
 
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 	

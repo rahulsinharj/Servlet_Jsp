4e9e70065760a5b1de1762b9221a239c96c72310
Rahul Sinha <rahulkumarsinha1234@gmail.com>
2022-05-08 15:55:59 +0530

    77

--- MyJsp2JSTL/src/main/java/jsp2jstl/DemoServlet.java
+++ MyJsp2JSTL/src/main/java/jsp2jstl/DemoServlet.java
@@ -11,10 +11,6 @@ import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
-/*
-	  
-*/
-
 @WebServlet("/demo")
 public class DemoServlet extends HttpServlet {
 	
